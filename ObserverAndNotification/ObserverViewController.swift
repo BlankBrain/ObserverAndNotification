26a7e9f7717ca192505544837756c471a3082b7b
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-02-04 15:48:17 +0600

    #first

---
+++ ObserverAndNotification/ObserverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ObserverViewController.swift
+//  ObserverAndNotification
+//
+//  Created by Md. Mehedi Hasan on 4/2/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ObserverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
