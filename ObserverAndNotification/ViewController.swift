9f7ff60f0918fad28eedd6a2588c723aae1ce7d8
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-02-04 15:45:50 +0600

    Initial Commit

---
+++ ObserverAndNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObserverAndNotification
+//
+//  Created by Md. Mehedi Hasan on 4/2/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
